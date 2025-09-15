1cb5c2da9ad203b299dceedabfa46baa9aca5ea9
Zachary D. Kline <klinez@tcnj.edu>
2025-09-15 11:10:35 -0400

    Add files via upload

---
+++ GSA.R
@@ -0,0 +1,18 @@
+# Project:   Grocery Store Access (GSA)
+# Located:   Google Drive
+# File Name: GSA
+# Date:      2025_9_8
+# Who:       Josh Sanchez and Zachary D. Kline
+# What:      Script for analysis of CPS data
+
+###############################################################################
+#################            STEP 1: SETTINGS             #####################
+###############################################################################
+# NOTE: To load data, you must download both the extract's data and the DDI
+# and also set the working directory to the folder with these files (or change the path below).
+
+#install.packages('ipumsr')
+library(ipumsr)
+
+ddi <- read_ipums_ddi("cps_00001.xml")
+data <- read_ipums_micro(ddi)

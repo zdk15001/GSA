95f6f48d46e5c4734ac433f3573ed448505de6de
Joshua-Sanchez <sanchej6@tcnj.edu>
2025-09-25 13:55:33 -0400

    Update GSA.R

--- GSA.R
+++ GSA.R
@@ -115,8 +115,16 @@ table(cps$FSFDSTMP, cps$food_stamp, useNA = "ifany")
 #Step 1: examine
 table(cps$FAMINC)
 
-#Step 2: clean
-cps$fam_income <- cps$FAMINC
+# Step 2: clean
+cps <- mutate(cps, less_than50k = ifelse(FAMINC <= 740, 1,
+                                    ifelse(FAMINC >= 995, NA, 0)))
+cps <- mutate(cps, more_than50k = ifelse(FAMINC >= 800 & FAMINC <= 994, 1,
+                                    ifelse(FAMINC >= 995, NA, 0)))
+#cps$more_than50k <- ifelse(cps$FAMINC >= 800 & cps$FAMINC <= 994, 1,
+                           #ifelse(cps$FAMINC >=995, NA, 0))
+# Step 3: confirm
+table(cps$FAMINC, cps$less_than50k, useNA = "ifany")
+table(cps$FAMINC, cps$more_than50k, useNA = "ifany")
 
 ##### Clean age
 # Step 1: examine
@@ -127,19 +135,59 @@ table(cps$AGE)
 table(cps$SEX)
 
 # Step 2: clean
-cps$sex <- cps$SEX
+cps$male <- ifelse(cps$SEX == 1, 1, 0)
+cps$female <- ifelse(cps$SEX == 2, 1, 0)
+
+# Step 3: confirm
+table(cps$SEX, cps$male, useNA = "ifany")
+table(cps$SEX, cps$female, useNA = "ifany")
 
 ##### Clean race
 # Step 1: examine
 table(cps$RACE)
 
+#Step 2: clean
+cps$white <- ifelse(cps$RACE == 100, 1, 0)
+cps$black <- ifelse(cps$RACE == 200, 1, 0)
+cps$native <- ifelse(cps$RACE == 300, 1, 0)
+cps$asian <- ifelse(cps$RACE == 651, 1, 0)
+cps$mixed <- ifelse(cps$RACE >= 652, 1, 0)
+
+# Step 3: confirm
+table(cps$RACE, cps$white, useNA = "ifany")
+table(cps$RACE, cps$black, useNA = "ifany")
+table(cps$RACE, cps$native, useNA = "ifany")
+table(cps$RACE, cps$asian, useNA = "ifany")
+table(cps$RACE, cps$mixed, useNA = "ifany")
+
+
 ##### Clean marital status
 # Step 1: examine
 table(cps$MARST)
 
+#Step 2: clean
+cps$married <- ifelse(cps$MARST == 1 | cps$MARST == 2, 1, 0)
+cps$not_married <- ifelse(cps$MARST >= 3 & cps$MARST <= 7, 1,
+                          ifelse(cps$MARST == 9, NA,))
+
+#Step 3: confirm
+table(cps$MARST, cps$married, useNA = "ifany")
+table(cps$MARST, cps$not_married, useNA = "ifany")
+
+
 ##### Clean employment status
 # Step 1: examine
 table(cps$EMPSTAT)
+
+# Step 2: clean
+cps$ILF <- ifelse(cps$EMPSTAT >= 01 & cps$EMPSTAT < 12, 1, 0)
+cps$NILF <- ifelse(cps$EMPSTAT >= 12, 1,
+                   ifelse(cps$EMPSTAT == 00, NA, 0))
+
+# Step 3: confirm 
+table(cps$EMPSTAT, cps$ILF, useNA = "ifany")
+table(cps$EMPSTAT, cps$NILF, useNA = "ifany")
+
 
 
 
